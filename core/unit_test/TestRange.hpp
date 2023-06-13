3b3d75acc4027c4bfed647c5caca374418a9ae82
tcclevenger <tccleve@sandia.gov>
2023-06-13 13:09:48 -0600

    f

--- core/unit_test/TestRange.hpp
+++ core/unit_test/TestRange.hpp
@@ -168,6 +168,10 @@ struct TestRange {
   //----------------------------------------
 
   void test_reduce() {
+        int const concurrency = ExecSpace().concurrency();
+
+    printf("CONCURRENCY: %d\n", concurrency);
+
     value_type total = 0;
 
     Kokkos::parallel_for(Kokkos::RangePolicy<ExecSpace, ScheduleType>(0, N),

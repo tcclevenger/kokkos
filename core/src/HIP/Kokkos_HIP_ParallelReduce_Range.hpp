6b28933eb34e8dd7e6f4a7f697c228935819394d
tcclevenger <tccleve@sandia.gov>
2023-12-13 11:10:58 -0700

    Revert "f"
    This reverts commit 830320cbbcde41ce99577ed0305fbe923ca8c509.

--- core/src/HIP/Kokkos_HIP_ParallelReduce_Range.hpp
+++ core/src/HIP/Kokkos_HIP_ParallelReduce_Range.hpp
@@ -157,7 +157,7 @@ class ParallelReduce<CombinedFunctorReducerType, Kokkos::RangePolicy<Traits...>,
       }
 
       for (unsigned i = threadIdx.y; i < word_count.value; i += blockDim.y) {
-	Kokkos::printf("  Read shared: i=%d, Tx=%d, Ty=%d, size=%d\n",i,threadIdx.x,threadIdx.y,sizeof(word_size_type));
+	Kokkos::printf("  Read shared: i=%d, Tx=%d, Ty=%d, Add=%p, size=%d\n",i,threadIdx.x,threadIdx.y,&(shared[i]),sizeof(word_size_type));
         global[i] = shared[i];
       }
     }
--- core/src/HIP/Kokkos_HIP_ReduceScan.hpp
+++ core/src/HIP/Kokkos_HIP_ReduceScan.hpp
@@ -185,7 +185,7 @@ struct HIPReductionsFunctor<FunctorType, false> {
           functor, my_shared_team_buffer_element, false,
           blockDim.x * blockDim.y / HIPTraits::WarpSize);
       if (threadIdx.x + threadIdx.y == 0) {
-	Kokkos::printf("  Write result: Tx=%d, Ty=%d, size=%d, skip=%s\n",threadIdx.x,threadIdx.y,sizeof(Scalar),skip?"T":"F");
+	Kokkos::printf("  Write result: Tx=%d, Ty=%d, Add=%p, size=%d, skip=%s\n",threadIdx.x,threadIdx.y,&(*result),sizeof(Scalar),skip?"T":"F");
         *result = *shared_team_buffer_element;
         if (skip) __threadfence();
       }

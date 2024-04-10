fa815353150ec613e4f526d1a4024c537c170699
tcclevenger <tccleve@sandia.gov>
2024-04-10 13:21:40 -0600

    WIP

--- core/unit_test/cuda/TestCuda_InterOp_StreamsMultiGPU.cpp
+++ core/unit_test/cuda/TestCuda_InterOp_StreamsMultiGPU.cpp
@@ -160,6 +160,9 @@ struct TestViewCudaAccessible {
     m_v0 = V(Kokkos::view_alloc("v0", mem_spaces[0]), N);
     m_v1 = V(Kokkos::view_alloc("v1", mem_spaces[1]), N);
 
+
+    printf("NAME: %s", MemSpace().name());
+return;
     if (MemSpace().name() == "CudaHostPinned") {
       Kokkos::parallel_for(
           Kokkos::RangePolicy<typename MemSpace::execution_space, TagInit>(0, N),

679494068141d97403d11d8bd3e7b45d2514a06e
tcclevenger <tccleve@sandia.gov>
2024-04-11 09:51:26 -0600

    WIP

--- core/unit_test/cuda/TestCuda_InterOp_StreamsMultiGPU.cpp
+++ core/unit_test/cuda/TestCuda_InterOp_StreamsMultiGPU.cpp
@@ -151,6 +151,7 @@ struct TestCudaSpace {
       MemSpace::impl_create(execs[1].cuda_device(), execs[1].cuda_stream())
     };
     m_v0 = V(Kokkos::view_alloc("v0", mem_spaces[0]), N);
+    std::cout << execs[1].cuda_device() << std::endl;
     m_v1 = V(Kokkos::view_alloc("v1", mem_spaces[1]), N);
 
     // Initialize each view (on correct device)

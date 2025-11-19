c16b1b06bda54495e803a42563a0b97bfa94e605
Conrad Clevenger <tccleve@sandia.gov>
2025-11-18 18:19:38 -0700

    Fix warning macro
    Signed-off-by: Conrad Clevenger <tccleve@sandia.gov>

--- core/src/Kokkos_Macros.hpp
+++ core/src/Kokkos_Macros.hpp
@@ -614,19 +614,13 @@ static_assert(
 #define KOKKOS_IMPL_DISABLE_UNREACHABLE_WARNINGS_POP()
 #endif
 
-#if defined(__NVCOMPILER)
+#if defined(__NVCC__)
 #define KOKKOS_IMPL_DISABLE_CALLING_HOST_FROM_DEVICE_WARNINGS_PUSH() \
-  _Pragma("diag_suppress 20011")          \
-  _Pragma("diag_suppress 20013")          \
-  _Pragma("diag_suppress 20014")          \
-  _Pragma("diag_suppress 20015")
+  _Pragma("nv_diag_suppress 20011, 20013, 20014, 20015")
 #define KOKKOS_IMPL_DISABLE_CALLING_HOST_FROM_DEVICE_WARNINGS_POP() \
-  _Pragma("diag_default 20011")          \
-  _Pragma("diag_default 20013")          \
-  _Pragma("diag_default 20014")          \
-  _Pragma("diag_default 20015")
+  _Pragma("nv_diag_default 20011, 20013, 20014, 20015")
 #else
-#define KOKKOS_IMPL_DISABLE_CALLING_HOST_FROM_DEVICE_WARNINGS_PUSH()
+#define KOKKOS_IMPL_DISABLE_CALLING_HOST_FROM_DEVICE_WARNINGS_PUSH() \
 #define KOKKOS_IMPL_DISABLE_CALLING_HOST_FROM_DEVICE_WARNINGS_POP()
 #endif
 // clang-format on

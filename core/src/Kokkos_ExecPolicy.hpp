a5e0f1e06b830517dce40f67e0e28c687a07d500
Conrad Clevenger <tccleve@sandia.gov>
2025-11-24 16:11:15 -0500

    Get work_tag explicitly from traits
    Signed-off-by: Conrad Clevenger <tccleve@sandia.gov>

--- core/src/Kokkos_ExecPolicy.hpp
+++ core/src/Kokkos_ExecPolicy.hpp
@@ -339,7 +339,7 @@ class ImplRangePolicy<ExecSpace, Properties...>
    */
   struct WorkRange {
     using work_tag =
-        typename ImplRangePolicy<ExecSpace, Properties...>::work_tag;
+        typename ImplRangePolicy<ExecSpace, Properties...>::traits::work_tag;
     using member_type =
         typename ImplRangePolicy<ExecSpace, Properties...>::member_type;
 

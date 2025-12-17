14fc17cf989026679e1746a36c67311b5767a2f1
Conrad Clevenger <tccleve@sandia.gov>
2025-12-17 15:28:52 -0500

    Assert inside specs that traits::execution_type is correct
    Signed-off-by: Conrad Clevenger <tccleve@sandia.gov>

--- core/src/Kokkos_ExecPolicy.hpp
+++ core/src/Kokkos_ExecPolicy.hpp
@@ -59,6 +59,7 @@ class ImplRangePolicy<ExecSpace, Properties...>
     : public Impl::PolicyTraits<Properties...> {
  public:
   using traits = Impl::PolicyTraits<Properties...>;
+  static_assert(std::same_as<typename traits::execution_type, ExecSpace>);
 
  private:
   typename traits::execution_space m_space;
@@ -1329,6 +1330,7 @@ class ImplRangePolicy<Handle, Properties...>
   using base_t::base_t;
 
   using traits = typename Impl::PolicyTraits<Properties...>;
+  static_assert(std::same_as<typename traits::execution_type, Handle>);
 
   using team_handle      = typename traits::team_handle;
   using member_type      = typename traits::index_type;

a51eb9b9b9675d25fca8d7d8cbebf46ccb666c0d
Conrad Clevenger <tccleve@sandia.gov>
2026-03-16 15:30:27 -0400

    wip

--- core/unit_test/default/TestDefaultDeviceDevelop.cpp
+++ core/unit_test/default/TestDefaultDeviceDevelop.cpp
@@ -14,21 +14,34 @@ import kokkos.core;
 
 namespace Test {
 
-template <size_t Padding>
 struct layout_right_padded_1 {
   template <class Extents>
   class mapping;
 };
 
-template <size_t Padding>
 template <class Extents>
-class layout_right_padded_1<Padding>::mapping {
+class layout_right_padded_1::mapping {
  public:
   using extents_type = Extents;
   using index_type   = typename extents_type::index_type;
   using size_type    = typename extents_type::size_type;
   using rank_type    = typename extents_type::rank_type;
   using layout_type  = layout_right;
+
+  KOKKOS_INLINE_FUNCTION
+  constexpr const extents_type& extents() const noexcept { return m_extents; }
+
+  KOKKOS_FUNCTION
+  constexpr index_type operator()(Indices... idxs) const noexcept { return 0; }
+
+  KOKKOS_INLINE_FUNCTION
+  constexpr index_type required_span_size() const noexcept {
+    index_type value = 1;
+    for (rank_type r = 0; r != extents_type::rank(); ++r) {
+      if (r ==) value *= m_extents.extent(r);
+    }
+    return value;
+  }
 
   KOKKOS_INLINE_FUNCTION static constexpr bool is_always_unique() noexcept {
     return true;
@@ -45,7 +58,7 @@ class layout_right_padded_1<Padding>::mapping {
   KOKKOS_INLINE_FUNCTION static constexpr bool is_unique() noexcept {
     return true;
   }
-  KOKKOS_INLINE_FUNCTION constexpr bool is_exhaustive() const noexcept {
+  KOKKOS_INLINE_FUNCTION bool is_exhaustive() const noexcept {
     return (extents_type::rank() < 2) || (exts.extent(1) == m_padded_stride);
   }
   KOKKOS_INLINE_FUNCTION static constexpr bool is_strided() noexcept {
@@ -55,22 +68,23 @@ class layout_right_padded_1<Padding>::mapping {
   KOKKOS_INLINE_FUNCTION constexpr index_type stride(
       rank_type r) const noexcept {
     assert(r < extents_type::rank());
-    if (r == extents_type::rank() - 1) return index_type(1);
-
     index_type value = 1;
-    for (rank_type k = extents_type::rank() - 2; k > r; k--)
-      value *= m_extents.extent(k);
-    if (r == 0) value *= m_padded_stride;
-
+    for (rank_type r = extents_type::rank() - 1; r > i; r--) {
+      if (r == 1)
+        value *= m_extents.extent(r) * m_padding_value;
+      else
+        value *= m_extents.extent(r);
+    }
     return value;
   }
 
   KOKKOS_INLINE_FUNCTION
   template <class OtherExtents>
-    requires Extents::rank() ==
-  OtherExtents::rank() friend constexpr bool operator==(
-      mapping const& lhs, mapping<OtherExtents> const& rhs) noexcept {
-    return lhs.extents() == rhs.extents();
+    requires(Extents::rank() == OtherExtents::rank())
+  friend constexpr bool operator==(mapping const& lhs,
+                                   mapping<OtherExtents> const& rhs) noexcept {
+    return lhs.extents() == rhs.extents() &&
+           lhs.m_padded_stride == rhs.m_padded_stride;
   }
 
  private:
